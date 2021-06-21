850014a75ed63b57a57c1682b80d5a5998cb7851
bigtittygothgirlfriend <eugen.haidarli@gmail.com>
2021-06-21 21:39:14 +0300

    nested the serializers trying to make the ratings wotk in the review serializer

--- cheeses/serializers.py
+++ cheeses/serializers.py
@@ -2,29 +2,36 @@ from rest_framework import serializers
 from .models import Cheese, Review, Rating
 from django.contrib.auth.models import User
 
-class CheeseSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = Cheese
-        fields = ['url', 'id', 'name', 'description', 'firmness', 'owner']
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     cheeses = serializers.HyperlinkedRelatedField(many=True, view_name='cheese-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'cheeses']
+        fields = ['url', 'id', 'username', 'cheeses', 'reviews', 'ratings']
 
-class ReviewSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-
-    class Meta:
-        model = Review
-        fields = ['url', 'id', 'cheese', 'content', 'score', 'owner']
-
-class RatingSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+class RatingSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
 
     class Meta:
         model = Rating
         fields = ['url', 'id', 'review', 'vote', 'owner']
+
+class ReviewSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    ratings = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Review
+        fields = ['url', 'id', 'cheese', 'content', 'score', 'owner', 'ratings']
+
+    def get_ratings(self, obj):
+        return obj.ratings
+
+
+class CheeseSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cheese
+        fields = ['url', 'id', 'name', 'description', 'firmness', 'owner', 'reviews']
