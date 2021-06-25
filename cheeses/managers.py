a2c226ecdc04fc024c1d1ef559af8c31025c7d50
bigtittygothgirlfriend <eugen.haidarli@gmail.com>
2021-06-25 17:53:57 +0300

    added all the Cheese and Review methods

--- cheeses/managers.py
+++ cheeses/managers.py
@@ -1,24 +1,42 @@
 from django.db import models
+from django.db.models import Count
 
 class CheeseQuerySet(models.QuerySet):
-    def get_users_cheese(self, username):
-        return self.filter(owner__username=username)
+    def get_cheeses_for_users(self, users):
+        return self.filter(owner__in=users)
     
-    def get_top_cheese(self, size):
-        return self.order_by('-reviews')
-
-    def test(self):
-        return self.filter(reviews__cheese=2)
+    def get_top_cheese(self, size=5):
+        return self.all().annotate(reviews_count=Count('reviews')).order_by('-reviews_count')[:size]
 
 class CheeseManager(models.Manager):
     def get_queryset(self):
         return CheeseQuerySet(self.model, using=self._db)
 
-    def get_users_cheese(self, username):
-        return self.get_queryset().get_users_cheese(username)
+    def get_cheeses_for_users(self, users):
+        return self.get_queryset().get_cheeses_for_users(users)
 
     def get_top_cheese(self, size):
         return self.get_queryset().get_top_cheese(size)
 
-    def test(self):
-        return self.get_queryset().test()
+class ReviewQuerySet(models.QuerySet):
+    def get_reviews_for_score(self, score):
+        return self.filter(score=score)
+
+    def get_reviews_by_content(self, string):
+        return self.filter(content__icontains=string)
+
+    def get_top_review(self, cheese):
+        return self.filter(cheese__name=cheese).annotate(ratings_count=Count('ratings__vote' == 1)).order_by('-ratings_count')[:1]
+
+class ReviewManager(models.Manager):
+    def get_queryset(self):
+        return ReviewQuerySet(self.model, using=self._db)
+
+    def get_reviews_for_score(self, score):
+        return self.get_queryset().get_reviews_for_score(score)
+
+    def get_reviews_by_content(self, string):
+        return self.get_queryset().get_reviews_by_content(string)
+
+    def get_top_review(self, cheese):
+        return self.get_queryset().get_top_review(cheese)
